cd90099b137010aeb7bf2f9903ab794da9ce22e0
Hodgparjor <olekbrzost@gmail.com>
2022-11-01 13:43:41 +0100

    Constructors, TreeMap, rid off nullPointerException
    Created some test cases, started implementing TreeMap for storing them,  Ensured NullPointerException can't occur, Added two constructors: One for empty object, the other one for initialising  each value.
    Changed variable name "lastName" into "surname"

--- Task 6/task6.java
+++ Task 6/task6.java
@@ -1,8 +1,30 @@
-
+import java.util.*;
 
 public class task6 {
     public static void main(String[] arg) {
-        System.out.println("Elo");
+        Person p1 = new Person("Marek", "Sczesny", "Kijanki Male", "Klasztorna",
+                       "29/31", "12-345", new TelephoneNumber("48", "501506353"));
+        Person p2 = new Person("Katarzyna", "Malinowska", "Piskornica", "Targowa",
+                       "3B", "53-985", new TelephoneNumber("48", "296175970"));
+        Person p3 = new Person("Kretek", "Pepik", "Brno", "Syrowa",
+                       "8", "88-747", new TelephoneNumber("420", "884668101"));
+        Company c1 = new Company("Kredkopol", "Lodz", "Zgierska", "294A",
+                                 "90-525", new TelephoneNumber("48", "123456789"));
+        Company c2 = new Company("Durr", "Hamburg", "Kleine-strasse",
+                         "77", "11-888", new TelephoneNumber("42", "096964383"));
+        // p1.description();
+        // p2.description();
+        // p3.description();
+        // c1.description();
+        // c2.description();
+
+        TreeMap<TelephoneNumber, TelephoneEntry> directory = new TreeMap<TelephoneNumber, TelephoneEntry>();
+        directory.put(p1.getTelephoneNumber(), p1);
+        directory.put(p2.getTelephoneNumber(), p2);
+        directory.put(p3.getTelephoneNumber(), p3);
+        directory.put(c1.getTelephoneNumber(), c1);
+        directory.put(c2.getTelephoneNumber(), c2);
+        System.out.println(directory);
     }
 }
 
@@ -92,39 +114,89 @@ abstract class TelephoneEntry {
 
 // Class Person has to contain information about the name, last name, and address (including TelephoneNumber)
 class Person extends TelephoneEntry {
-    private String  lastName;
+    private String  surname;
+
+    Person(String name, String surname, String city, String street, String streetNumber, String postalCode, TelephoneNumber telephoneNumber) {
+        this.setName(name);
+        this.setSurname(surname);
+        this.setCity(city);
+        this.setStreet(street);
+        this.setStreetNumber(streetNumber);
+        this.setPostalCode(postalCode);
+        this.setTelephoneNumber(telephoneNumber);
+    }
+
+    Person() {
+        this.setName("-");
+        this.setSurname("-");
+        this.setCity("-");
+        this.setStreet("-");
+        this.setStreetNumber("-");
+        this.setPostalCode("-");
+        this.setTelephoneNumber(new TelephoneNumber("-", "-"));
+    }
 
     @Override
     public void description() {
-        System.out.println("------------------------------\n"
-                         + "           PERSON\n"
-                         + "Name: " + this.getName() + "\n"
-                         + "Last name: " + lastName + "\n"
-                         + "Addres:\n"
-                         + "  City: " + this.getCity() + "\n"
-                         + "  Street: " + this.getStreet() + this.getStreetNumber() + "\n"
-                         + "  Postal code: " + this.getPostalCode() + "\n"
-                         + "  Telephone Number: +" + this.getTelephoneNumber().getCountryCode() + " " + this.getTelephoneNumber().getLocalNumber()
-                         + "\n------------------------------");
+        if (this.getName() != null && this.getSurname() != null && this.getCity() != null && this.getStreet() != null
+            && this.getStreetNumber() != null && this.getPostalCode() != null && this.getTelephoneNumber() != null
+            && this.getTelephoneNumber().getCountryCode() != null && this.getTelephoneNumber().getLocalNumber() != null) {
+            System.out.println("------------------------------\n"
+                            + "           PERSON\n"
+                            + "Name: " + this.getName() + "\n"
+                            + "Surname: " + surname + "\n"
+                            + "Addres:\n"
+                            + "  City: " + this.getCity() + "\n"
+                            + "  Street: " + this.getStreet() + " " + this.getStreetNumber() + "\n"
+                            + "  Postal code: " + this.getPostalCode() + "\n"
+                            + "  Telephone: +" + this.getTelephoneNumber().getCountryCode() + " " + this.getTelephoneNumber().getLocalNumber()
+                            + "\n------------------------------");
+        }
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 }
 // Class Company has to have the name and address (including TelephoneNumber)
 class Company extends TelephoneEntry {
+
+    Company(String name, String city, String street, String streetNumber, String postalCode, TelephoneNumber telephoneNumber) {
+        setName(name);
+        setCity(city);
+        setStreet(street);
+        setStreetNumber(streetNumber);
+        setPostalCode(postalCode);
+        setTelephoneNumber(telephoneNumber);
+    }
+
+    Company() {
+        this.setName("-");
+        this.setCity("-");
+        this.setStreet("-");
+        this.setStreetNumber("-");
+        this.setPostalCode("-");
+        this.setTelephoneNumber(new TelephoneNumber("-", "-"));
+    }
+
     @Override
     public void description() {
-        System.out.println("------------------------------\n"
-                         + "           COMPANY\n"
-                         + "Name: " + this.getName() + "\n"
-                         + "Addres:\n"
-                         + "  City: " + this.getCity() + "\n"
-                         + "  Street: " + this.getStreet() + this.getStreetNumber() + "\n"
-                         + "  Postal code: " + this.getPostalCode() + "\n"
-                         + "  Telephone Number: +" + this.getTelephoneNumber().getCountryCode() + " " + this.getTelephoneNumber().getLocalNumber()
-                         + "\n------------------------------");
-        
+        if (this.getName() != null && this.getCity() != null && this.getStreet() != null
+            && this.getStreetNumber() != null && this.getPostalCode() != null && this.getTelephoneNumber() != null
+            && this.getTelephoneNumber().getCountryCode() != null && this.getTelephoneNumber().getLocalNumber() != null) {
+            System.out.println("------------------------------\n"
+                            + "           COMPANY\n"
+                            + "Name: " + this.getName() + "\n"
+                            + "Addres:\n"
+                            + "  City: " + this.getCity() + "\n"
+                            + "  Street: " + this.getStreet() + " " + this.getStreetNumber() + "\n"
+                            + "  Postal code: " + this.getPostalCode() + "\n"
+                            + "  Telephone Number: +" + this.getTelephoneNumber().getCountryCode() + " " + this.getTelephoneNumber().getLocalNumber()
+                            + "\n------------------------------");
+        }
     }
 }
